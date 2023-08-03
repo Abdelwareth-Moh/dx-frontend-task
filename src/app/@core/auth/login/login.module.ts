0aae556ed298bbb1f0c1462d73bac283abaccd4e
Abdewsareth Said <Abdelwareth.Said@dx-republic.com>
2023-08-03 16:21:21 +0300

    login-page

--- src/app/@core/auth/login/login.module.ts
+++ src/app/@core/auth/login/login.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './pages/login/login.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [FormsModule, CommonModule, LoginRoutingModule, ReactiveFormsModule],
 })
-export class LoginModule { }
+export class LoginModule {}
--- src/app/@core/auth/login/pages/login/login.component.ts
+++ src/app/@core/auth/login/pages/login/login.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private router: Router) {
+    localStorage.removeItem('register');
+  }
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+  login() {
+    //     this.adminService.login(this.loginForm.value).subscribe((data:any)=>{
+    //       localStorage.setItem('token',data.token)
+    //       localStorage.setItem('role',data.role[0].name)
+    //       if(localStorage.getItem('role')=='Admin')
+    //     {
+    //       this.router.navigate(['/admin-dashboard/products'])
+    //     }
+    //     else
+    //     {      this.router.navigate(['/admin-dashboard'])
+    // }
+    //     },
+    //     (error:any)=>{
+    //     }
+    //     )
+  }
 }
