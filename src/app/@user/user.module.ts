8e1e057713006f71f423602b5809bd00c136d745
Abdewsareth Said <Abdelwareth.Said@dx-republic.com>
2023-08-03 16:46:48 +0300

    user-components

--- src/app/@user/user.module.ts
+++ src/app/@user/user.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { CategoryProductsComponent } from './pages/category-products/category-products.component';
+import { ProductDetailesComponent } from './pages/product-detailes/product-detailes.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CategoriesComponent,
+    CategoryProductsComponent,
+    ProductDetailesComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
