59cca4405830e93f9db58c0abdb595dbbb31d4c5
Abdewsareth Said <Abdelwareth.Said@dx-republic.com>
2023-08-03 10:17:40 +0300

    layout-folder

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-
+import { NavbarComponent } from './components/layout-components/navbar/navbar.component';
+import { FooterComponent } from './components/layout-components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    NavbarComponent,
-    FooterComponent
-  ],
-  imports: [
-    CommonModule,
-    CoreRoutingModule
-  ]
+  declarations: [NavbarComponent, FooterComponent],
+  imports: [CommonModule, CoreRoutingModule],
 })
-export class CoreModule { }
+export class CoreModule {}
