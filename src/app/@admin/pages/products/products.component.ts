2db5bb482bd36f99e0355b84639c3acaa7bc860b
Abdewsareth Said <Abdelwareth.Said@dx-republic.com>
2023-08-06 17:20:07 +0300

    products-actions

--- src/app/@admin/pages/products/products.component.ts
+++ src/app/@admin/pages/products/products.component.ts
@@ -1,8 +1,24 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/app/@core/services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent {}
+export class ProductsComponent {
+  products: any[] = [];
+  constructor(private _productService: ProductService) {}
+  ngOnInit() {
+    this.callGetProducts();
+  }
+
+  callGetProducts() {
+    this._productService.getProducts().subscribe(
+      (data: any) => {
+        this.products = data;
+      },
+      (err: any) => {}
+    );
+  }
+}
